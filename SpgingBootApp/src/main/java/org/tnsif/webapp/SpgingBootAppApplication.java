393c1d67cbdb80481475997c303c1a16833cd6e7
atharva-123 <atharvashirole1234@gmail.com>
2023-04-11 10:50:39 +0530

    First Commit

---
+++ SpgingBootApp/src/main/java/org/tnsif/webapp/SpgingBootAppApplication.java
@@ -0,0 +1,13 @@
+package org.tnsif.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpgingBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpgingBootAppApplication.class, args);
+	}
+
+}
